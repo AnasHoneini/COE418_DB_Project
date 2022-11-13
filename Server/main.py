893c125476e1c5fadd8eed432e994b5c13cd3776
Anas Honeini <anas.hneineh@gmail.com>
2022-11-13 11:50:16 +0200

    test on backend

--- Server/main.py
+++ Server/main.py
@@ -1,1 +1,15 @@
-print("Hello Word")
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'hello world'
+
+@app.route('/home')
+def home():
+    return render_template('index1.html')   
+
+
+if __name__=="__main__":
+        app.run(debug=True)
