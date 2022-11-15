d8787b4047e2cfaff53272ae34380a838c181be2
Anas Honeini <anas.hneineh@gmail.com>
2022-11-15 17:21:25 +0200

    backend

--- Server/main.py
+++ Server/main.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
 
-app= Flask(__name__)
+app= Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route('/')
 def hello_world():
-    return 'hello world'
+    return render_template('index.html') 
 
 @app.route('/home')
 def home():
-    return render_template('index1.html')   
+      return 'hello world'
 
+@app.route('/login')
+def login():
+    return render_template('login.html')      
 
 if __name__=="__main__":
         app.run(debug=True)
+        
