a3a0208ed7f671e7b18115b98d29e3564333ee92
Anas Honeini <anas.hneineh@gmail.com>
2022-11-21 01:13:45 +0200

    fixes

--- Server/app/views.py
+++ Server/app/views.py
@@ -5,14 +5,21 @@ from app import app
 from flask import Flask, render_template
 
 @app.route('/')
-def hello_world():
-    return render_template('public/index.html') 
-
+def home():
+    return render_template('public/templates/home.html') 
 
 @app.route('/login')
 def login():
-    return render_template('/public/login.html') 
+    return render_template('/public/templates/login.html') 
 
 @app.route('/signup')
 def signup():
-    return render_template('public/signup.html')     
+    return render_template('/public/templates/signup.html')     
+
+@app.route('/booking')
+def booking():
+    return render_template('/public/templates/booking.html') 
+
+@app.route('/booking-room')
+def booking_room():
+    return render_template('/public/templates/booking_room.html')     
