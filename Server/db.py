a07d72d2f07a706a59f12fd82d41f46937bdd6cb
PamelaKaram <pamela.karam01@lau.edu>
2022-12-03 00:42:19 +0200

    Final touch on homepage

--- Server/db.py
+++ Server/db.py
@@ -2,10 +2,10 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
     host="localhost",
-    port=3307,
+    port=3306,
     database='HospitalManagement',
     user="root",
-    password="Anas@1234"
+    password="1234"
 )
 
 my_cursor = mydb.cursor(buffered=True)
