8b22088337bd948246bc4f0e3254be0652c0b045
Anas Honeini <anas.hneineh@gmail.com>
2022-12-19 07:05:01 +0200

    k

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -1,11 +1,10 @@
-# file to tell python that this is a package
-from app import admin_views
-from app import views
+#file to tell python that this is a package
 import db
 from flask import Flask
 
 
 app = Flask(__name__)
-app.secret_key = 'anasistheadmin'
+app.secret_key ='anasistheadmin'
 
-# import any file from the package
+#import any file from the package
+from app import views 
